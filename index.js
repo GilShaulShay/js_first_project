9ef1d074e7fe0037c7b99b2e7b6568ab2da8311f
GilShay <gilshayu@gmail.com>
2022-02-16 23:55:09 +0200

    add first button

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ console.log('Hello World');
 let name = 'Gil';
 console.log(name);
 
-const interstRate =0.3;
-interstRate = 1;
-console.log(interstRate);
+var button = document.getElementById("button");
+
+button.onclick = function() {
+    const interstRate =0.3;
+    console.log(interstRate);   
+};
